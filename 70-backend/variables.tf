c98ae5f5459c06ace230211b3384923157793e57
rajikakani412 <raji.kakani@gmail.com>
2024-10-17 20:05:59 +0530

    updated

---
+++ 70-backend/variables.tf
@@ -0,0 +1,22 @@
+variable "project_name" {
+  default = "expense"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "common_tags" {
+  default = {
+    Project     = "expense"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+variable "backend_tags"{
+  default={
+    component= "backend"
+  }
+}
+
